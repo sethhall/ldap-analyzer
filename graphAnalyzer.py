492b5a548846228f36aff8ecd17f56ba4f1da652
Montir <ruhutbutar@gmail.com>
2017-07-14 16:38:15 -0400

    Create graphAnalyzer.py

---
+++ graphAnalyzer.py
@@ -0,0 +1,13 @@
+import networkx as nx
+import pandas as pd
+import sys
+import sqlite3
+
+ldap = sqlite3.connect("/var/db/ldap.sqlite")
+c = ldap.cursor()
+G = nx.Graph()
+
+target = ""
+
+c = 
+#graph the network connections
